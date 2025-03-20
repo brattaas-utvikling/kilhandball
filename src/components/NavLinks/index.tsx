d3eaf55c94916da695b89ba00fc7896925dc9715
Jon Are <jon.are.br@gmail.com>
2025-03-20 14:30:50 +0100

    navlinks styling

--- src/components/NavLinks/index.tsx
+++ src/components/NavLinks/index.tsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 function NavLinks({ toggleMenu }: { toggleMenu: () => void }) {
   return (
-    <ul className="flex flex-col mt-7 w-full d:mt-0 md:flex-row gap-4 md:gap-6 md:items-center">
+    <ul className="flex flex-col mt-7 w-full md:mt-0 md:flex-row gap-4 md:gap-6 md:items-center">
       {[
         { path: '/', label: 'Hjem' },
         { path: '/om-oss', label: 'Om oss' },
@@ -13,10 +13,10 @@ function NavLinks({ toggleMenu }: { toggleMenu: () => void }) {
           <NavLink
             to={path}
             className={({ isActive }) =>
-              `block w-full text-center font-roboto after:text-lg px-4 py-2 transition-all duration-100
+              `block w-full text-center font-roboto after:text-lg px-4 py-2 transition-all duration-100 md:h-full md:flex md:items-center
               ${isActive
-                ? 'md:text-white md:rounded-lg md:border-b-2 md:border-e-2 md:border-white md:bg-transparent bg-white text-kilred'
-                : 'text-white hover:bg-white hover:text-kilred md:hover:bg-transparent md:hover:text-white md:hover:rounded-lg md:hover:border-b-2 md:hover:border-e-2 md:hover:border-white'
+                ? ' md:rounded-lg md:border-b-2 md:border-t-0 md:border-white md:bg-kilred md:text-white bg-white text-kilred'
+                : 'text-white hover:bg-white hover:text-kilred md:hover:bg-transparent md:hover:text-white md:hover:rounded-lg md:hover:border-b-2 md:hover:border-t-0 md:hover:border-white'
               }`
             }
             onClick={() => toggleMenu()}
